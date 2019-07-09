b4e1d4901f18c8493f122dcba358ea748734de51
Aminata Danioko <37707013+amdanioko@users.noreply.github.com>
2019-07-09 18:57:06 -0400

    FindNumber in C#

---
+++ FindNumber/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindNumber
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 6);
+            int count = 1;
+
+            Console.WriteLine("Guess the Secret Number:");
+
+            while (true)
+            {
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if (guess > randomNumber)
+
+                {
+                    Console.WriteLine("Too large.Try again");
+                    count++;
+                } else if (guess < randomNumber)
+                    count++;
+
+                { Console.WriteLine("Too small.Try again");
+                    count++;
+
+            } else if (guess == randomNumber) 
+            
+            {
+                Console.WriteLine("Thats correct you found the number!");
+                Console.WriteLine($"This is your number of tries{count}");
+            }
+
+
+          
+            }
+        }
+    }
+}
